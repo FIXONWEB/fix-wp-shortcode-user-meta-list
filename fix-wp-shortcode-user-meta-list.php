b992614f7aab043cc9e92fcbf924979a7ff1f44d
fixonweb <contato@fixonweb.com.br>
2020-04-17 12:15:09 -0300

    commit 2 - ajustes iniciais

---
+++ fix-wp-shortcode-user-meta-list.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Plugin Name:     Fix User Meta List
+ * Plugin URI:      https://github.com/fixonweb/fix-wp-shortcode-user-meta-list
+ * Description:     Listagem de usuarios.
+ * Author:          FIXONWEB
+ * Author URI:      https://github.com/fixonweb
+ * Text Domain:     fix-wp-shortcode-user-meta-list
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Fix_Wp_Shortcode_User_Meta_List
+ */
+
+/* Código de identificação deste plugin fix158713 */
+
+/* ATUALIZAÇÃO DESTE PLUGIN VIA GITHUB */
+require 'plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/fixonweb/backoffice',
+	__FILE__, 
+	'backoffice/backoffice'
+);
+
